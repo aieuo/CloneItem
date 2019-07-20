f2c0c3696d2926b27861427dfbc8412b0d559cec
aieuo <35859665+aieuo@users.noreply.github.com>
2019-07-20 12:29:43 +0900

    複製する数、複製先のプレイヤー、複製元のプレイヤーを指定できるように

--- src/aieuo/cloneItem.php
+++ src/aieuo/cloneItem.php
@@ -1,22 +1,46 @@
 <?php
 namespace aieuo;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class cloneItem extends PluginBase implements Listener{
+class cloneItem extends PluginBase {
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+    	if(!isset($args[2]) and !($sender instanceof Player)) return false;
 
-    public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this,$this);
-    }
+        $from = $sender;
+        $to = $sender;
 
-    public function onCommand(CommandSender $sender, Command $command,string $label, array $args):bool{
-    	if(!($sender instanceof Player))return false;
-    	$sender->getInventory()->addItem($sender->getInventory()->getItemInHand());
-    	$sender->sendMessage("複製しました");
+        if(isset($args[2])) {
+            $from = $this->getServer()->getPlayer($args[2]);
+            if(!($from instanceof Player)) {
+                $sender->sendMessage($args[2]."はサーバーにいません");
+                return true;
+            }
+        }
+
+        if(isset($args[1])) {
+            $to = $this->getServer()->getPlayer($args[1]);
+            if(!($to instanceof Player)) {
+                $sender->sendMessage($args[1]."はサーバーにいません");
+                return true;
+            }
+        }
+
+        $item = $from->getInventory()->getItemInHand();
+
+        if(isset($args[0])) {
+            if(!is_numeric($args[0]) or (int)$args[0] <= 0) {
+                $sender->sendMessage("複製する数は1以上の数字で入力してください");
+                return true;
+            }
+            $item->setCount((int)$args[0]);
+        }
+
+    	$to->getInventory()->addItem($item);
+    	$sender->sendMessage($from->getName()."のアイテムを".$item->getCount()."個".$to->getName()."に複製しました");
         return true;
     }
 }
